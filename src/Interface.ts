35617d13796ead5ebcedd4468aee5e638a9b55f1
Clovis Fong <fongclovis@hotmail.com>
2022-10-09 17:10:53 +0800

    revised formula for income graph to account for different income status

--- src/Interface.ts
+++ src/Interface.ts
@@ -1,3 +1,18 @@
-// export interface xx {
+export interface IIncomeData {
+    amount: number;
+    created_at: string;
+    duration_months: number;
+    frequency: string;
+    growth_rate: number;
+    id: number;
+    income_name: string;
+    income_status: string;
+    income_type: string;
+    start_date: string;
+    updated_at: string;
+}
 
-// }
+export interface IIncomeGraphInputs {
+    age: number;
+    income: number
+}
--- src/components/SavingsGraph.tsx
+++ src/components/SavingsGraph.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react'
-import { format, parse, differenceInCalendarYears, getMonth } from 'date-fns'
+import { differenceInCalendarYears } from "date-fns"
+import IncomeGraph from "./IncomeGraph"
+
 
 
 const SavingsGraph = () => {
-
-    const incomeGraphInputs: any = []
 
     const userDetails = {
         created_at: "Sat, 08 Oct 2022 15:14:00 GMT",
@@ -21,104 +20,67 @@ const SavingsGraph = () => {
         updated_at: "Sat, 08 Oct 2022 15:14:42 GMT"
     }
 
-    const incomeData = {
-        amount: 3000,
-        created_at: "Sun, 09 Oct 2022 02:41:29 GMT",
-        duration_months: 10,
-        frequency: "Semi-Annually",
-        growth_rate: 1.5,
-        id: 22,
-        income_name: "Stockie",
-        income_status: "Current",
-        income_type: "Investment",
-        start_date: "Sun, 09 Oct 2022 00:00:00 GMT",
-        updated_at: "Sun, 09 Oct 2022 02:41:29 GMT"
-    }
-
-
+    // General details: Current age, retirement age, life-expectancy age
     const birthDate = new Date(userDetails.date_of_birth)
     const currentDate = new Date // use current date
-
     const currentAge = differenceInCalendarYears(currentDate, birthDate) // 24
     const yearsToRetirement = userDetails.retirement_age - currentAge //42
     const yearsToLifeExpectancy = userDetails.life_expectancy - currentAge //66
 
+    const incomeTimeline = []
 
-    if (incomeData.income_status === 'Current') {
-        // Initial Age: Output: 24
-        const dateofIncomeEntry = new Date(incomeData.created_at)
-
-
-        // Remaining months till year end
-        const monthOfEntry = getMonth(dateofIncomeEntry)
-        const initialYearRemainingMonths = 12 - monthOfEntry // 3 months
-        const initialYearAge = differenceInCalendarYears(dateofIncomeEntry, birthDate)
-        const futureIncomeMonths = incomeData.duration_months - initialYearRemainingMonths //117
-        const finalYear = Math.ceil(futureIncomeMonths / 12) // 10
-
-
-        const storeInitialYearValue = (period: number) => {
-            if (incomeData.frequency === 'Monthly') {
-                incomeGraphInputs.push({ "Age": initialYearAge, "Income": period * incomeData.amount })
-            } else if (incomeData.frequency === 'Quarterly') {
-                incomeGraphInputs.push({ "Age": initialYearAge, "Income": Math.ceil(period / 3) * incomeData.amount })
-            } else if (incomeData.frequency === 'Semi-Annually') {
-                incomeGraphInputs.push({ "Age": initialYearAge, "Income": Math.ceil(period / 6) * incomeData.amount })
-            } else if (incomeData.frequency === 'Annually') {
-                incomeGraphInputs.push({ "Age": initialYearAge, "Income": incomeData.amount })
-            }
-        }
-
-        if (futureIncomeMonths > 0) {
-            // Initial Year Income
-            storeInitialYearValue(initialYearRemainingMonths) // {age: 24, Income : 9000}
-
-
-            const storeFutureYearsValue = (numOfYear: number, numOfMonths: number) => {
-                if (incomeData.frequency === 'Monthly') {
-                    incomeGraphInputs.push({ "Age": initialYearAge + numOfYear, "Income": Math.ceil(numOfMonths * incomeData.amount * Math.pow((1 + (incomeData.growth_rate / 100)), numOfYear)) })
-                } else if (incomeData.frequency === 'Quarterly') {
-                    incomeGraphInputs.push({ "Age": initialYearAge + numOfYear, "Income": Math.ceil(Math.ceil(numOfMonths / 3) * incomeData.amount * Math.pow((1 + (incomeData.growth_rate / 100)), numOfYear)) })
-                } else if (incomeData.frequency === 'Semi-Annually') {
-                    incomeGraphInputs.push({ "Age": initialYearAge + numOfYear, "Income": Math.ceil(Math.ceil(numOfMonths / 6) * incomeData.amount * Math.pow((1 + (incomeData.growth_rate / 100)), numOfYear)) })
-                } else if (incomeData.frequency === 'Annually') {
-                    incomeGraphInputs.push({ "Age": initialYearAge + numOfYear, "Income": Math.ceil(incomeData.amount * Math.pow((1 + (incomeData.growth_rate / 100)), numOfYear)) })
-                }
-            }
-
-            if (futureIncomeMonths >= 12) {
-                const fullYearIncomeMonths = futureIncomeMonths - (12 - initialYearRemainingMonths) //108
-                const fullYearIncomeYears = Math.floor(fullYearIncomeMonths / 12) // 9 years
-
-                // Following Full-Years Income Projections
-                for (let year = 1; year <= fullYearIncomeYears; year++) {
-                    storeFutureYearsValue(year, 12)
-                }
-
-                // Final Year Income Projections
-                const finalYearIncomeMonths = incomeData.duration_months - initialYearRemainingMonths - fullYearIncomeMonths
-                storeFutureYearsValue(finalYear, finalYearIncomeMonths)
-            }
-
-            else if (futureIncomeMonths < 12) {
-                // Final Year Income Projections
-                const FinalYearIncomeMonths = incomeData.duration_months - initialYearRemainingMonths
-                storeFutureYearsValue(finalYear, FinalYearIncomeMonths)
-
-            }
-
-        } else if (futureIncomeMonths <= 0) {
-            storeInitialYearValue(incomeData.duration_months)
-        }
-
+    for (let age = currentAge; age <= userDetails.life_expectancy; age++) {
+        incomeTimeline.push({ "age": age, "totalIncome": 0 })
     }
 
+    console.log(incomeTimeline)
 
-    console.log(incomeGraphInputs)
+
+
+    const incomeData = [
+        {
+            amount: 3000,
+            created_at: "Sun, 09 Oct 2022 08:45:57 GMT",
+            duration_months: 120,
+            frequency: "Monthly",
+            growth_rate: 1.5,
+            id: 12,
+            income_name: "Stocks",
+            income_status: "Future",
+            income_type: "Investment",
+            start_date: "Thu, 10 Oct 2024 00:00:00 GMT",
+            updated_at: "Sun, 09 Oct 2022 08:45:57 GMT"
+        },
+        {
+            amount: 7000,
+            created_at: "Sun, 09 Oct 2022 08:38:13 GMT",
+            duration_months: 504,
+            frequency: "Monthly",
+            growth_rate: 2.0,
+            id: 11,
+            income_name: "Working Income",
+            income_status: "Current",
+            income_type: "Salary",
+            start_date: "Sun, 09 Oct 2022 00:00:00 GMT",
+            updated_at: "Sun, 09 Oct 2022 08:38:13 GMT"
+        }
+    ]
+
+
+
+    const incomeProjections = incomeData.map(income => IncomeGraph(income))
+    console.log(incomeProjections)
+
+
+
 
 
     return (
-        <div>SavingsGraph</div>
+        <>
+            <div>SavingsGraph</div>
+
+        </>
+
     )
 }
 
