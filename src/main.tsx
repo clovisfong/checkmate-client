3675d5c11041625ca1664b3ef307c4285f5c7069
Clovis Fong <fongclovis@hotmail.com>
2022-10-26 17:17:49 +0800

    fixed bugs

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from '@emotion/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import FinancialProvider from './components/contextStore/FinancialProvider'
 import UserDetailsProvider from './components/contextStore/UserDetailsProvider'
 import './index.css'
 import theme from './theme'
@@ -11,9 +10,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <UserDetailsProvider>
-        <FinancialProvider>
-          <App />
-        </FinancialProvider>
+        <App />
       </UserDetailsProvider>
     </ThemeProvider>
   </React.StrictMode>
